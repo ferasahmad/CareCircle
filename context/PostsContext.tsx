c4d5c2137b116f3890348b193b917ec8ea1cc9b9
Feras Ahmad <firastaf878@gmail.com>
2024-09-17 13:53:11 -0400

    change: use react context

---
+++ context/PostsContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useContext } from "react";
+import { useFetchPosts } from "@/hooks/useFetchPosts"; // Import your hook
+
+interface PostsContextProps {
+  posts: Post[];
+  loading: boolean;
+  error: string | null;
+  fetchPosts: () => void;
+}
+
+const PostsContext = createContext<PostsContextProps | undefined>(undefined);
+
+export const usePosts = () => {
+  const context = useContext(PostsContext);
+  if (!context) {
+    throw new Error("usePosts must be used within a PostsProvider");
+  }
+  return context;
+};
+
+export const PostsProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { posts, loading, error, fetchPosts } = useFetchPosts();
+
+  return (
+    <PostsContext.Provider value={{ posts, loading, error, fetchPosts }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
