a41b0b47dd0662d114bf439d391b550bdcfff5d4
Feras Ahmad <firastaf878@gmail.com>
2024-09-18 16:47:50 -0400

    change: handle empty comments state

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text, StyleSheet } from "react-native";
 import Comment from "./Comment";
 
 interface CommentsListProps {
@@ -14,29 +15,42 @@ const CommentsList: React.FC<CommentsListProps> = ({
   expandedComments,
   toggleCollapse,
 }) => {
+  const hasComments = Object.keys(comments).length > 0;
+
   return (
     <>
-      {Object.values(comments).map((comment) => (
-        <Comment
-          key={comment.id}
-          comment={comment}
-          level={level}
-          isCollapsed={!expandedComments[comment.id]}
-          toggleCollapse={toggleCollapse}
-          renderReplies={() =>
-            comment.replies ? (
-              <CommentsList
-                comments={comment.replies}
-                level={level + 1}
-                expandedComments={expandedComments}
-                toggleCollapse={toggleCollapse}
-              />
-            ) : null
-          }
-        />
-      ))}
+      {!hasComments ? (
+        <Text style={styles.noCommentsText}>No comments yet</Text>
+      ) : (
+        Object.values(comments).map((comment) => (
+          <Comment
+            key={comment.id}
+            comment={comment}
+            level={level}
+            isCollapsed={!expandedComments[comment.id]}
+            toggleCollapse={toggleCollapse}
+            renderReplies={() =>
+              comment.replies ? (
+                <CommentsList
+                  comments={comment.replies}
+                  level={level + 1}
+                  expandedComments={expandedComments}
+                  toggleCollapse={toggleCollapse}
+                />
+              ) : null
+            }
+          />
+        ))
+      )}
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  noCommentsText: {
+    alignSelf: "center",
+    color: "#343434",
+  },
+});
 
 export default CommentsList;
