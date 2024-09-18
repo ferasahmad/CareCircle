4bd4904defa1fb68a6527b1f498cd2d3830535db
Feras Ahmad <firastaf878@gmail.com>
2024-09-18 15:06:53 -0400

    changes: ui changes and new components

--- app/post-details.tsx
+++ app/post-details.tsx
@@ -1,27 +1,18 @@
 import { useLocalSearchParams } from "expo-router";
 import React, { useState } from "react";
 import { usePosts } from "@/context/PostsContext";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  Alert,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, StyleSheet, ScrollView, Alert } from "react-native";
 import GenericWrapper from "@/components/GenericWrapper";
-import Markdown from "react-native-markdown-display";
-import { LinearGradient } from "expo-linear-gradient";
 import formatDate from "@/utilities/formatDate";
 import PostAction from "@/components/PostAction";
 import { useHugPost } from "@/hooks/useHugPost";
 import CommentsList from "@/components/CommentList";
+import ChatbotAssessment from "@/components/ChatbotAssesment";
 
 export default function PostDetails() {
   const { postId } = useLocalSearchParams<{ postId: string }>();
   const { posts, loading, error } = usePosts();
 
-  const [isExpanded, setIsExpanded] = useState(false);
   const [expandedComments, setExpandedComments] = useState<
     Record<number, boolean>
   >({});
@@ -32,7 +23,12 @@ export default function PostDetails() {
 
   const { toggleHugPost, error: hugError } = useHugPost();
 
-  if (!post) return <Text>Post not found</Text>;
+  if (!post)
+    return (
+      <GenericWrapper>
+        <Text>Post not found</Text>
+      </GenericWrapper>
+    );
 
   const handleHugPress = async () => {
     const updatedPost = await toggleHugPost(post.id, numHugs);
@@ -53,49 +49,32 @@ export default function PostDetails() {
 
   return (
     <GenericWrapper>
-      <ScrollView contentContainerStyle={{ padding: 15, gap: 15 }}>
-        <View style={[styles.cardContainer, styles.postContent]}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 15 }}>
+        <View style={styles.cardContainer}>
           <Text style={styles.title}>{post.title}</Text>
           <Text style={styles.description}>{post.patient_description}</Text>
-          <View style={styles.actionsContainer}>
-            <PostAction
-              imageSrc={
-                hasHugged
-                  ? require("../assets/images/heart-filled.png")
-                  : require("../assets/images/heart.png")
-              }
-              label={numHugs}
-              onPress={handleHugPress}
-            />
-            <PostAction
-              imageSrc={require("../assets/images/comment.png")}
-              label="Comment"
-              onPress={() => {}}
-            />
+          <View>
+            <View style={styles.actionsContainer}>
+              <PostAction
+                imageSrc={
+                  hasHugged
+                    ? require("../assets/images/heart-filled.png")
+                    : require("../assets/images/heart.png")
+                }
+                label={numHugs}
+                onPress={handleHugPress}
+              />
+              <PostAction
+                imageSrc={require("../assets/images/comment.png")}
+                label="Comment"
+                onPress={() => {}}
+              />
+            </View>
             <Text style={styles.date}>{formatDate(post.created_at)}</Text>
           </View>
         </View>
-        <View style={[styles.cardContainer, styles.assessmentContainer]}>
-          <Text style={styles.title}>Chatbot Assessment</Text>
-          <Markdown>
-            {isExpanded
-              ? post.assessment
-              : post.assessment.split("\n").slice(0, 5).join("\n")}
-          </Markdown>
-          {!isExpanded && (
-            <TouchableOpacity
-              onPress={() => setIsExpanded(true)}
-              style={styles.expandButton}
-            >
-              <LinearGradient
-                colors={["transparent", "#E6F4FC"]}
-                style={styles.gradient}
-              />
-              <Text style={styles.expandText}>Read More</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-        <View style={[styles.commentsContainer, styles.cardContainer]}>
+        <ChatbotAssessment assessment={post.assessment} />
+        <View style={styles.cardContainer}>
           <Text style={styles.title}>Comments</Text>
           <CommentsList
             comments={post.comments}
@@ -109,69 +88,33 @@ export default function PostDetails() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    backgroundColor: "#fff",
-  },
   cardContainer: {
     padding: 15,
-    gap: 15,
-    borderRadius: 15,
+    marginBottom: 20,
+    backgroundColor: "#FEFAE1",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 3,
   },
-  postContent: {
-    backgroundColor: "#FFE4E4",
-  },
   title: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 10,
   },
   description: {
-    fontSize: 14,
+    fontSize: 16,
     color: "#555",
   },
   date: {
     fontSize: 14,
-    color: "#343434",
+    color: "#777",
+    marginTop: 5,
   },
   actionsContainer: {
     flexDirection: "row",
-    alignItems: "center",
     justifyContent: "space-between",
-  },
-  assessmentContainer: {
-    position: "relative",
-    backgroundColor: "#E6F4FC",
-  },
-  expandButton: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    alignItems: "center",
-    justifyContent: "center",
-    height: 70,
-  },
-  gradient: {
-    position: "absolute",
-    borderRadius: 15,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 70,
-  },
-  expandText: {
-    color: "#000",
-    fontSize: 14,
-    fontWeight: "bold",
-  },
-  commentsContainer: {
-    gap: 15,
-    paddingVertical: 10,
-    backgroundColor: "#EAE6FC",
+    marginVertical: 10,
   },
 });
